a6642501baf308cc122d690770f49d1621d45772
ShinjeongA <jeonga@khu.ac.kr>
2021-11-18 18:23:53 +0900

    App.js에서 라우팅을 통해 각 컴포넌트들로 들어가기

--- react_from_noma/src/App.js
+++ react_from_noma/src/App.js
@@ -1,73 +1,27 @@
-import Button from "./Button";//내가 만들 Button.js에서 Button을 가져옴
-import styles from "./App.module.css";
-import {useState,useEffect} from "react";//useState,juseEffect import
-
-
-function Hello(){
-  useEffect(()=>{//처음에 컴포넌트가 만들어질 때 호출됨
-    console.log("I'm created");
-    //clean up function: component가 destroy 될 때 뭔가 해줄 수 있게 해줌
-    return ()=>console.log("I'm destroyed");// destroy될 떄 불러올 내용
-    //useEffect는 해당 컴포넌트가 destory 될 때 return 값을 리턴한다.
-  },[])
-  return(
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//npm i react-router-dom@5.3.0를 해주고 해야 함
+//Link는 브라우저 새로 고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포넌트임
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  //create react app을 사용하기 때문에 React.useState라고 적어줄 필요 없음 , 그냥 useState라고 해주면 됨
-  const [counter,setValue]=useState(0);
-  const[keyword,setKeyword]=useState("");
-  const[showing, setShowing]=useState(false);
-
-  const onClick=()=>setValue((prev)=>prev+1);
-  const onClick2=()=>setShowing((prev)=>!prev);
-  console.log("I run all the time");
-  useEffect(()=>{
-    console.log("call the API");
-  },[]);
-
-  const onChange=(event)=>{//입력이 발생하면 event로 그 입력값을 받아온다!!!]
-    //그리고 입력값을 keyword 변수에 저장함
-    setKeyword(event.target.value)
-  }
-
-  const iRunOnlyOnce=()=>{
-    console.log("I run only once");
-  }
-  useEffect(iRunOnlyOnce,[]);//useEffect는 첫 번째 인자가 제일 처음에만,최초의 렌더링일 때만 렌더링되고 이후로는 더이상 렌더링되지 않도록 한다
-
-  //이렇게 useEffect의 두 번째 인자에 []안에 state(변수)를 넣으면 그 state에 변화가 있을 때만 useState의 첫 번째 인자가 렌더링됨
-  //즉 특정한 state가 업데이트 될 때만 어떤 코드를 실행하고 싶을 때
-  useEffect(()=>{
-    if(keyword!==""&&keyword.length>5){
-      console.log("I run when 'keyword' changes");
-    }
-  },[keyword]);
-
-  useEffect(()=>{
-    console.log("I run when 'counter' changes");
-  },[counter]);
-
-  //두 조건(시점)을 합칠 수도 있음, 조건을 여러 개 둘 수도 있음
-  useEffect(()=>{
-    console.log("I run when 'counter'&'counter' changes");
-  },[keyword,counter]);
-
   return (
-    <div>
-      <h1 className={styles.title}>WELCOME BACK!!!</h1>
-      <input  value={keyword} onChange={onChange} type="text" placeholder="Search here"/>
-       {/*!!onChange는 그냥 props 이름 {}안에 들어간 onChange가 (콜백)함수이름 */}
-       {/*value는 입력창에 보여지게 함. 즉 value 덕분에 화면의 입력창과 state 및 입력값이 연결되는 것임 */}
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-      {showing?<Hello/>:null}
-      <button onClick={onClick2}>{showing?"Hide":"Show"}</button>
-    </div>
+    <Router> {/*react-app 측에서 미리 제공하는 컴포넌트임*/}
+      <Switch> {/*Swtich는 라우트를 찾는 역할. route는 url에서 https://domain:portnumber/...에서 /...부분을 의미함 */}
+        {/*라우트를 찾으면 컴포넌트를 렌더링함, React Router는 다이나믹(동적) url 을 지원해줌-다이나믹: url에 변수를 넣을 수 있다는 의미 */}
+        <Route path="/hello"> 
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id"> {/*근데 이 route가 path="/" 라우트 밑에 있으면 /movie해도 실행 안 됨. 루트 라우트 위에 있어야 함 */}
+        {/*React Router는 다이나믹(동적) url 을 지원해줌-다이나믹: url에 변수를 넣을 수 있다는 의미 , 
+        주의할 점: path="/movie/:id" 이렇게 나타내야 함. : 없이 그냥 path="/movie/id" 이렇게 해버리면 그냥 스트링 그대로 url을 쳐야 함*/}
+          <Detail />
+        </Route>
+        <Route path="/"> {/*루트 라우터가 젤 밑에 있어야 함*/ }
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
