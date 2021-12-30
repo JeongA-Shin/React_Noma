59d575c72caa71b14554190c4a87358c8bf18300
ShinjeongA <jeonga@khu.ac.kr>
2021-12-30 21:33:36 +0900

    라우터

--- react_from_noma/src/App.js
+++ react_from_noma/src/App.js
@@ -6,18 +6,26 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router> {/*react-app 측에서 미리 제공하는 컴포넌트임*/}
-      <Switch> {/*Swtich는 라우트를 찾는 역할. route는 url에서 https://domain:portnumber/...에서 /...부분을 의미함 */}
+    <Router>
+      {" "}
+      {/*react-app 측에서 미리 제공하는 컴포넌트임*/}
+      <Switch>
+        {" "}
+        {/*Swtich는 라우트를 찾는 역할. route는 url에서 https://domain:portnumber/...에서 /...부분을 의미함 */}
         {/*라우트를 찾으면 컴포넌트를 렌더링함, React Router는 다이나믹(동적) url 을 지원해줌-다이나믹: url에 변수를 넣을 수 있다는 의미 */}
-        <Route path="/hello"> 
+        <Route path="/hello">
           <h1>Hello</h1>
         </Route>
-        <Route path="/movie/:id"> {/*근데 이 route가 path="/" 라우트 밑에 있으면 /movie해도 실행 안 됨. 루트 라우트 위에 있어야 함 */}
-        {/*React Router는 다이나믹(동적) url 을 지원해줌-다이나믹: url에 변수를 넣을 수 있다는 의미 , 
+        <Route path="/movie/:id">
+          {" "}
+          {/*근데 이 route가 path="/" 라우트 밑에 있으면 /movie해도 실행 안 됨. 루트 라우트 위에 있어야 함 */}
+          {/*React Router는 다이나믹(동적) url 을 지원해줌-다이나믹: url에 변수를 넣을 수 있다는 의미 , 
         주의할 점: path="/movie/:id" 이렇게 나타내야 함. : 없이 그냥 path="/movie/id" 이렇게 해버리면 그냥 스트링 그대로 url을 쳐야 함*/}
           <Detail />
         </Route>
-        <Route path="/"> {/*루트 라우터가 젤 밑에 있어야 함*/ }
+        <Route path="/">
+          {" "}
+          {/*루트 라우터가 젤 밑에 있어야 함!!!!!!!!- 텀프하다가 이거 몰라서 엄청 헤맴*/}
           <Home />
         </Route>
       </Switch>
